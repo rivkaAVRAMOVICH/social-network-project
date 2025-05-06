a1c355a3f587cda10496c8a7554795bb1424a981
yehudit@009 <judith.sabag1@gmail.com>
2025-05-06 15:22:30 +0300

    create DAL todos

---
+++ src/server/DAL/todos.js
@@ -0,0 +1,58 @@
+const db = require('../../db/connection');
+
+// Get all todos
+async function getAllTodos(user_id) {
+  const [rows] = await db.query('SELECT * FROM myappdb.todos WHERE user_id = ?',[user_id]);
+  console.log(JSON.stringify(rows))
+  return rows;
+}
+
+// Get a todo by ID
+async function getTodoById(id) {
+  const [rows] = await db.query('SELECT * FROM myappdb.todos  WHERE id = ?', [id]);
+//   console.log(JSON.stringify(rows[0]));
+  return rows[0];
+}
+
+// Add a new todo
+async function addTodo(todo) {
+  const { user_id, title, completed = false } = todo;
+  const [result] = await db.query(
+    'INSERT INTO myappdb.todos  (user_id, title, completed) VALUES (?, ?, ?)',
+    [user_id, title, completed]
+  );
+  return { id: result.insertId, ...todo };
+}
+
+// Update a todo
+async function updateTodo(id, todo) {
+  const { title, completed, } = todo;
+  const [result] = await db.query(
+    `UPDATE myappdb.todos SET title = ?, completed = ? WHERE id = ?`,
+    [title, completed, id]
+  );
+  console.log(JSON.stringify(result));
+
+  return result.affectedRows > 0;
+}
+
+// Delete a todo
+async function deleteTodo(id) {
+  const [result] = await db.query('DELETE FROM myappdb.todos  WHERE id = ?', [id]);
+  return result.affectedRows > 0;
+}
+
+async function deleteAllTodos(user_id) {
+    const [result] = await db.query('DELETE FROM myappdb.todos  WHERE user_id = ?', [user_id]);
+    return result.affectedRows > 0;
+  }
+  
+
+module.exports = {
+  getAllTodos,
+  getTodoById,
+  addTodo,
+  updateTodo,
+  deleteTodo,
+  deleteAllTodos
+};
