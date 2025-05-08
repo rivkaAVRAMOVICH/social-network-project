159bd7f3854c8d14452d3928803421d4f05cbd8f
rivkaAVRAMOVICH <mavramovich7@gmail.com>
2025-05-08 16:38:57 +0300

    update server

--- server/API/routes/deleteRouter.js
+++ server/API/routes/deleteRouter.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../controller');
 // מסלול GET שמחזיר רשימת משתמשים
-router.get('/:table', async (req,res)=>{
-    const { table } = req.params;
-    const controller = Controller.getController(table);
-  
-    if (!controller || !controller.deleteById) {
-      return res.status(404).json({ error: 'Entity not found' });
-    }
+router.delete('/:table/:id', async (req, res) => {
+  const { table, id } = req.params;
+  const controller = require(`../../BL/${table}Manager.js`);
 
-    try {
-      const data = await controller.deleteById(id);
-      res.json(data);
-    } catch (err) {
-      res.status(500).json({ error: 'Server error' });
-    }
+  if (!controller || !controller.deleteById) {
+    return res.status(404).json({ error: 'Entity not found' });
+  }
+
+  try {
+    const data = await controller.deleteById(id);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 module.exports = router;
